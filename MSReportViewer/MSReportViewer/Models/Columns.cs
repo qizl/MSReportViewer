bd0362ce1cb9c1e4e2be46245b22b3f6d4dbcca1
qizl <q@qizl.cn>
2017-03-30 14:56:09 +0800

    -

---
+++ MSReportViewer/MSReportViewer/Models/Columns.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSReportViewer.Models
+{
+    public class Columns
+    {
+        public Guid Name { get; set; }
+        public string Describe { get; set; }
+    }
+}
